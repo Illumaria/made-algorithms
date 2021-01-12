c69d0a0fd1786dbd9296bf431ecb4b06f44234ef
Dmitry Astankov <astankov.dmitry@gmail.com>
2021-01-12 12:39:05 +0300

    Change from C to C++ code style

--- 1_merge_sort.cpp
+++ 1_merge_sort.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 template <typename T, typename U>
-void merge(std::vector<T>& v, std::vector<T>& buf, U left, U mid, U right) {
+void Merge(std::vector<T>& v, std::vector<T>& buf, U left, U mid, U right) {
   U i = left, k = left;
   U j = mid + 1;
 
@@ -22,37 +22,40 @@ void merge(std::vector<T>& v, std::vector<T>& buf, U left, U mid, U right) {
 }
 
 template <typename T, typename U>
-void _merge_sort(std::vector<T>& v, std::vector<T>& buf, U left, U right) {
+void MergeSort_(std::vector<T>& v, std::vector<T>& buf, U left, U right) {
   if (left < right) {
     U mid = left + (right - left) / 2;
-    _merge_sort(v, buf, left, mid);
-    _merge_sort(v, buf, mid + 1, right);
-    merge(v, buf, left, mid, right);
+    MergeSort_(v, buf, left, mid);
+    MergeSort_(v, buf, mid + 1, right);
+    Merge(v, buf, left, mid, right);
   }
 }
 
 template <typename T>
-void merge_sort(std::vector<T>& v) {
+void MergeSort(std::vector<T>& v) {
   if (!v.empty()) {
     std::vector<T> buf(v.size());
-    _merge_sort(v, buf, (uint32_t)0, (uint32_t)(v.size() - 1));
+    MergeSort_(v, buf, static_cast<uint32_t>(0),
+               static_cast<uint32_t>(v.size() - 1));
   }
 }
 
 int main() {
+  std::iostream::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
   uint32_t n;
-  scanf("%lu", &n);
+  std::cin >> n;
 
   // Read values:
   std::vector<int64_t> v(n);
-  for (uint32_t i = 0; i < n; i++) scanf("%lld", &v[i]);
+  for (size_t i = 0; i < n; ++i) std::cin >> v[i];
 
   // Sort values:
-  merge_sort(v);
+  MergeSort(v);
 
   // Print values:
-  for (const auto& x : v) printf("%lld ", x);
-  printf("\n");
+  for (const auto& x : v) std::cout << x << " ";
 
   return 0;
 }
